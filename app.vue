1fdb25c22a80f146f286ab5e4aa182116363555c
WassimWeb <wassimbenmansour73@gmail.com>
2024-04-19 00:29:43 +0200

    set project

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Header />
+    <div>
+      sidebar
+    </div>
+    <main>
+      contain
+    </main>
+  </div>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="sticky top-0 z-20 border-b backdrop-blur">
+        <div class="container flex h-16 items-center justify-between">
+            <!--Logo-->
+            <div class="flex items-center gap-3">
+                <img src="/icons/logo.avif"  alt="Admin Dashboard" class="h-14 w-14 object-contain" />
+                <NuxtLink to="/" class="text-xl font-bold"> Admin Dashboard </NuxtLink>
+            </div>
+        </div>
+        <!--Right Side-->
+        <div class="flex items-center gap-5">
+            <button class="flex h-9 w-9 shrink-0 items-center justify-center rounded-full border">
+                
+            </button>
+        </div>
+    </header>
+</template>
+
+<script setup>
+</script>
